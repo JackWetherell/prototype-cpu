ff7b984cd1541d29bc18124f92876f9c1fa8a479
Jack Wetherell <jack.wetherell@gmail.com>
2023-01-09 21:28:01 +0000

    Working on EEPROM programmer code.

--- eeprom_programmer/eeprom_programmer.ino
+++ eeprom_programmer/eeprom_programmer.ino
@@ -5,18 +5,14 @@
 #define EEPROM_D7 12
 #define WRITE_ENABLE 13
 
-
 void setAddress(int address, bool output_enable)
 {
   shiftOut(SHIFT_DATA, SHIFT_CLOCK, MSBFIRST, (address >> 8) | (output_enable ? 0x00 : 0x80));
   shiftOut(SHIFT_DATA, SHIFT_CLOCK, MSBFIRST, address);
-  
   digitalWrite(SHIFT_LATCH, LOW);
   digitalWrite(SHIFT_LATCH, HIGH);
   digitalWrite(SHIFT_LATCH, LOW);
-  delay(10);
 }
-
 
 byte readEEPROM(int address)
 {
@@ -24,18 +20,15 @@ byte readEEPROM(int address)
   {
     pinMode(pin, INPUT);
   }
-  
   setAddress(address, true);
-  
   byte data = 0;
   for (int pin = EEPROM_D7; pin >= EEPROM_D0; pin -= 1)
   {
     data = (data << 1) + digitalRead(pin);
   }
-  
-  return data;
+  return data; 
+  delay(10);
 }
-
 
 void writeEEPROM(int address, byte data)
 {
@@ -43,32 +36,27 @@ void writeEEPROM(int address, byte data)
   {
     pinMode(pin, OUTPUT);
   }
-  
   setAddress(address, false);
-  
   for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin += 1)
   {
     digitalWrite(pin, data & 1);
     data = data >> 1;
   }
-  
   digitalWrite(WRITE_ENABLE, LOW);
   delayMicroseconds(1);
   digitalWrite(WRITE_ENABLE, HIGH);
   delay(10);
 }
 
-
 void printContents()
 {
-   for (int base = 0; base <= 255; base += 16)
+  for (int base = 0; base <= 255; base += 16)
   {
     byte data[16];
     for (int offset = 0; offset <= 15; offset += 1)
     {
       data[offset] = readEEPROM(base + offset);
     }
-
     char buf[80];
     sprintf(buf, "%03x: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x", 
                  base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
@@ -79,10 +67,15 @@ void printContents()
 
 void clearEEPROM()
 {
-   for(int address = 0; address <= 255; address += 1)
+  Serial.print("Erasing EEPROM");
+  for(int address = 0; address <= 2047; address += 1)
   {
     writeEEPROM(address, 0xff);
+    if (address % 64 == 0) {
+      Serial.print(".");
+    }
   }
+  Serial.println(" done.");
 }
 
 void setup()
@@ -94,14 +87,18 @@ void setup()
   pinMode(WRITE_ENABLE, OUTPUT);
   Serial.begin(57600);
   delay(1000);
-
   clearEEPROM();
-
-  byte data[] = {0x01, 0x4f, 0x12, 0x06, 0x4c, 0x24, 0x20, 0x0f, 0x00, 0x04, 0x08, 0x60, 0x31, 0x42, 0x30, 0x38};
-  for(int address = 0; address <= 15; address += 1)
+  Serial.print("Programming EEPROM");
+  // byte data[] = {0x01, 0x4f, 0x12, 0x06, 0x4c, 0x24, 0x20, 0x0f, 0x00, 0x04, 0x08, 0x60, 0x31, 0x42, 0x30, 0x38};
+  byte data[] = { 0x81, 0xcf, 0x92, 0x86, 0xcc, 0xa4, 0xa0, 0x8f, 0x80, 0x84, 0x88, 0xe0, 0xb1, 0xc2, 0xb0, 0xb8 };
+  for(int address = 0; address < sizeof(data); address += 1)
   {
     writeEEPROM(address, data[address]);
+    if (address % 64 == 0) {
+      Serial.print(".");
+    }
   }
+  Serial.println(" done.");
 
   printContents();
 }
